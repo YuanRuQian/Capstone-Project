222ffe8512119e24526ef56426a387faf33d1a63
Lydia Yuan <lydiayuan99@gmail.com>
2023-10-03 09:22:52 -0600

    chore: add testing notes

--- raft/netwrok_test.go
+++ raft/netwrok_test.go
@@ -1,5 +1,8 @@
 package raft
 
+// command line to see the goroutinue stack dump: go test -v -run TestElectionBasic -bench=. -cpuprofile=cpu.pprof -memprofile=mem.pprof
+// see the content at: http://localhost:6060/debug/pprof/goroutine?debug=2
+
 import (
 	"net/http"
 	_ "net/http/pprof"
